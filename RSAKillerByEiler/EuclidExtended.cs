82df60a1e584ac326bba9841da36ce93cd0e17a8
Woipot <frostik0409@gmail.com>
2019-04-23 22:00:42 +0300

    end task

---
+++ RSAKillerByEiler/EuclidExtended.cs
@@ -0,0 +1,45 @@
+﻿using System.Numerics;
+
+namespace RSAKillerByEiler
+{
+    public class EuclidExtended
+    {
+        private BigInteger a, b;
+
+        public EuclidExtended(BigInteger a, BigInteger b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Solve()
+        {
+            BigInteger x0 = 1, xn = 1, y0 = 0, yn = 0, x1 = 0, y1 = 1, f, r = a % b;
+
+            while (r > 0)
+            {
+                f = a / b;
+                xn = x0 - f * x1;
+                yn = y0 - f * y1;
+
+                x0 = x1;
+                y0 = y1;
+                x1 = xn;
+                y1 = yn;
+                a = b;
+                b = r;
+                r = a % b;
+            }
+
+            XN = xn;
+            YN = yn;
+            B = b;
+        }
+
+        public BigInteger B { get; set; }
+
+        public BigInteger YN { get; set; }
+
+        public BigInteger XN { get; set; }
+    }
+}
---
+++ RSAKillerByEiler/MainVM.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Documents;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace RSAKillerByEiler
+{
+    public class MainVM : BindableBase
+    {
+        public ObservableCollection<DataObject> List { get; }
+
+        public class DataObject
+        {
+            public string E { get; set; }
+            public string D { get; set; }
+        }
+
+        public string EilerValue { get; set; }
+
+        public DelegateCommand StartCommand { get; }
+
+
+        public MainVM()
+        {
+            List = new ObservableCollection<DataObject>();
+            StartCommand = new DelegateCommand(Start);
+        }
+
+        private void Start()
+        {
+            var isNum = BigInteger.TryParse(EilerValue, out var eilerValue);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Not valid Eiler Value");
+                return;
+            }
+
+
+
+            List.Clear();
+           
+            var eList = GetAllE(eilerValue);
+            
+            foreach (var e in eList)
+            {
+                var euclid = new EuclidExtended(e, eilerValue);
+                euclid.Solve();
+
+                List.Add(new DataObject() { E = e.ToString(), D = euclid.XN.ToString()});
+            }
+
+        }
+
+
+        private IEnumerable<BigInteger> GetAllE(BigInteger eilerValue)
+        {
+            var result = new List<BigInteger>();
+
+            for (BigInteger i = 2; i < eilerValue; i++)
+            {
+                var gcd = Gcd(i, eilerValue);
+                if (gcd == 1)
+                    result.Add(i);
+            }
+
+            return result;
+        }
+
+        private BigInteger Gcd(BigInteger first, BigInteger second)
+        {
+            while (first != 0 && second != 0)
+            {
+                if (first > second)
+                    first %= second;
+                else
+                    second %= first;
+            }
+
+            return first == 0 ? second : first;
+        }
+    }
+}
